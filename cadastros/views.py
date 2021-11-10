2ee2b475700da3d81f84e4c0cf78d1f677a547c2
lucas <lucas.lr184@gmail.com>
2021-11-09 22:37:11 -0300

    controle de acesso por grupos de usuario

--- cadastros/views.py
+++ cadastros/views.py
@@ -1,29 +1,39 @@
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from braces.views import GroupRequiredMixin
 from .models import Campo, Atividade
 
 # Create your views here.
-class CampoCreate(CreateView):
+class CampoCreate(GroupRequiredMixin,LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
+    group_required = u'Administrador'
     model = Campo
     fields = ['nome', 'descricao']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-campos')
 
-class AtividadeCreate(CreateView):
+class AtividadeCreate(GroupRequiredMixin,LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
+    group_required = u'Administrador'
     model = Atividade
     fields = ['numero', 'descricao', 'pontos', 'detalhes', 'campo']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-atividades')
 
 #UPDATE
-class CampoUpdate(UpdateView):
+class CampoUpdate(GroupRequiredMixin,LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
+    group_required = u'Administrador'
     model = Campo
     fields = ['nome', 'descricao']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-campos')
 
-class AtividadeUpdate(UpdateView):
+class AtividadeUpdate(GroupRequiredMixin,LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
+    group_required = u'Administrador'
     model = Atividade
     fields = ['numero', 'descricao', 'pontos', 'detalhes', 'campo']
     template_name = 'cadastros/form.html'
@@ -31,23 +41,30 @@ class AtividadeUpdate(UpdateView):
 
 
 #DELETE
-class CampoDelete(DeleteView):
+class CampoDelete(GroupRequiredMixin,LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
+    group_required = u'Administrador'
     model = Campo
     template_name = 'cadastros/form-delete.html'
     success_url = reverse_lazy('listar-campos')
 
-class AtividadeDelete(DeleteView):
+class AtividadeDelete(GroupRequiredMixin,LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
+    group_required = u'Administrador'
     model = Atividade
     template_name = 'cadastros/form-delete.html'
     success_url = reverse_lazy('index')
 
 
 #LISTAR
-class CampoList(ListView):
+class CampoList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     model = Campo
     template_name = 'cadastros/listas/campo.html'
+    
 
 
-class AtividadeList(ListView):
+class AtividadeList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     model = Atividade
     template_name = 'cadastros/listas/atividade.html'
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(
-        template_name='usuarios/form-login.html'
+        template_name='usuarios/form-check-auth.html'
     ), name='login'),   
     path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
